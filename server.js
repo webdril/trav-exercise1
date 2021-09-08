57d9f9779594103b6bc178c6ad09d0213d7f2458
webdril <jimbahamas15@gmail.com>
2021-09-08 05:48:20 -0500

    changes made

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const serverSpeed = process.env.SERVER_SPEED;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -5,14 +6,11 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`The current speed of our server is ${serverSpeed}`);
     await sleep(5000);
   }
 }
 
-export SERVER_SPEED = 7000
-const serverSpeed = process.env.SERVER_SPEED;
 
-console.log(`The current speed of our server is ${serverSpeed}`);
 
 main();
